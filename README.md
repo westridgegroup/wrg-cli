e67186be98914a275629d542dbededc4c0cf3821
Steve Fibich <steve.fibich@nowhere.com>
2025-02-26 07:42:40 -0500

    first client update

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# wrgcli
-Westridge Group client
+# wrgcli: 
+### Westridge Groups Client Enterprise grade built for the individual 
+
+## Intro 
+The terraform-azure-boostrap project provides an enterprise-ready backend using Azure resources.  Using the terraform-azure-bootstrap, moves your Terraform state files to Azure Storage and uses an App Registration Client Id and Client Secret for authorization to Azure.  This allows you to decouple the permissions from your account to that of the App Registration.  It also allows you to share state files across your organization with other developers or CICD pipelines.  Lastly, it allows you to release the same terraform code to different environments without overwriting state files and without modifying the terraform code or having some sort of pre-execution replacement script run.
+
+## Requirements
+
+- The AZ CLI
+- An Azure Subscription
+- Bash/Z Shell
+
+## Contents
+
+- [Intro](#intro)
+- [Requirements](#requirements)
+- [Inital Setup](#inital-setup)
+- [Bootstrapping](#bootstrapping)
+- [Scripts](#scripts)
+
+## Initial Setup 
+(once per subscription) 
+
+```{r, engine='sh', count_lines}
+az login --use-device-code
+./ConfigureAzureForSecureTerraformAccess.sh
+```
+
+## Bootstrapping
+The wrgcli provides an enterprise-ready backend for open-tofu using Azure resources.   Using the bootstraping mechanisms of the wrgcli, moves your open-tofu state files to Azure Storage and uses an App Registration Client Id and Client Secret for authorization to Azure.  This allows you to decouple the permissions from your account to that of the App Registration.  It also allows you to share state files across your organization with other developers or CICD pipelines.  Lastly, it allows you to release the same terraform code to different environments without overwriting state files and without modifying the terraform code or having some sort of pre-execution replacement script run.
+
+
+![wrgcli-azure-bootstrap](./wrgcli-azure-bootstrap.png)
+
+
+(per project/environment switch)
+
+```
+az login --use-device-code
+source wrgcli.sh
+tofu_setup env/dev.tfvars
+tofu_plan
+tofu_apply
+```
+
+## Scripts
+
+### ConfigureAzureForSecureTofuAccess.sh
+
+The script creates a resource group, a key vault, a storage account, and an SPN.  It loads the client id, client secret, tenant id, subscription id, and storage account access key into the key vault. Next, the script grants the current user ownership over the key vault. Finally, it uses the current subscription the user is logged into to create the resources.
+
+### TerraformAzureBootStrap.sh
+
+The script loads information based on the account logged into Azure.  It expects the default subscription to contain a key vault named terraform-kv and a storage account named something that ends in terraform all within the resource group called terraform-mgmt-rg.  The ConfigureAzureForSecureTerraformAccess creates these objects. So, if that script has been previously executed, the required objects should exist.  The script then pulls the required azurerm configuration information from the key vault.  It also expects a tfvars file passed to it, which is the environment file for the project.  This file defines the terraform azurerm container_name and key for terraform state information.  The same tfvars file can contain other environment-specific variables for the project as well.  Using this script allows you to use the same terraform script for multiple environments without changing remote state values or worrying about overwriting state information. 
---
+++ wrgcli.sh
@@ -0,0 +1,242 @@
+#!/bin/bash
+
+#############################################################################################################################
+#DESCRIPTION																												#
+#	Loads Azure Key Vault secrets into Tofu environment variables for the current bash session.								#
+#																															#
+#    The following steps are automated:																						#
+#    - Identifies the Azure Key Vault matching a search string (default: 'terraform-kv').									#
+#    - Retrieves the Terraform secrets from Azure Key Vault.																#
+#    - Loads the Terraform secrets into these environment variables for the current bash session:							#
+#        - ARM_SUBSCRIPTION_ID																								#
+#        - ARM_CLIENT_ID																									#
+#        - ARM_CLIENT_SECRET																								#
+#        - ARM_TENANT_ID																									#
+#        - ARM_ACCESS_KEY																									#
+#																															#
+#VERSION 3.0.0																												#
+#																															#
+#EXAMPLE																													#
+#    source ./wrgcli.sh																			#
+#                                                                             												#	
+#    Loads Azure Key Vault secrets into Terraform environment variables for the current bash session						#
+#																															#
+#NOTES																														#
+#    Assumptions:																											#
+#    - Az Cli install																										#
+#	 - You are inside a zsh/bash session																					#
+#    - You are already logged into Azure before running this script (eg. az account login)									#
+#	 - Opentufu is installed																								#
+#																															#
+#    Author:  SFibich																										#	
+#    GitHub:  https://github.com/westridgegroup																				#
+#																															#
+#    This script was modeled after Adam Rush's script LoadAzureTerraformSecretsToEnvVars.ps1 https://github.com/adamrushuk.	#
+#																															#
+#############################################################################################################################
+echo "sourcing wrgcli.sh"
+echo "tofu_help for help"
+
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+OPTIND=1
+while getopts :m:f:k:r:s: flag
+do
+    case "${flag}" in
+		f) ENV_FILE=${OPTARG};;
+        k) USER_KEY_VAULT_PATTERN=${OPTARG};;
+        r) USER_RESOURCE_GROUP=${OPTARG};;
+        s) USER_SUBSCRIPTION=${OPTARG};;
+		?) SKIP=TRUE 
+			echo "help: switches -f ENV_FILE -k USER_KEY_VAULT_PATTERN -r USER_RESOURCE_GROUP -s USER_SUBSCRIPTION" ;;
+    esac
+done
+
+alias faa=' (print -rlo ${(k)functions} ${(k)aliases} )'
+alias tofu_help=' (faa | grep ^tofu)'
+
+function terra_read_env_file() {
+	echo "ENV FILE: $ENV_FILE"
+	a=$(grep state_container_name $ENV_FILE)
+	b=$(echo $a | tr -d "=")
+	c=${b#state_container_name}
+	d=$(echo $c | tr -d '"')
+	e=${d##*( )};
+	STATE_CONTAINER_NAME=$e
+
+	f=$(grep state_key $ENV_FILE)
+	g=$(echo $f | tr -d "=")
+	h=${g#state_container_name}
+	i=$(echo $h | tr -d '"')
+	j=${i##*( )};
+	STATE_KEY=$j
+	
+	echo "STATE_CONTAINER_NAME: $STATE_CONTAINER_NAME"
+	echo "STATE_KEY: $STATE_KEY"
+}
+
+function terra_set_core_variables() {
+
+if [ -z "$USER_KEY_VAULT_PATTERN" ]
+	then
+		KEY_VAULT_NAME_PATTERN=terraform-kv
+		echo "Using Default KEY_VAULT_NAME_PATTERN:$KEY_VAULT_NAME_PATTERN"
+	else
+		KEY_VAULT_NAME_PATTERN=$USER_KEY_VAULT_PATTERN
+		echo "Using input KEY_VAULT_NAME_PATTERN: $KEY_VAULT_NAME_PATTERN"
+fi
+
+if [ -z "$USER_RESOURCE_GROUP" ]
+	then
+		TERRAFORM_RESOURCE_GROUP=terraform-mgmt-rg
+		echo "Using Default TERRAFORM_RESORUCE_GROUP:$TERRAFORM_RESOURCE_GROUP"
+	else
+		TERRAFORM_RESOURCE_GROUP=$USER_RESOURCE_GROUP
+		echo "Using input TERRAFORM_RESOURCE_GROUP:$TERRAFORM_RESOURCE_GROUP"
+fi
+
+
+}
+
+
+function terra_get_keyvault_values() {
+
+#####################
+#Check Azure login	#
+#####################
+echo "Checking for an active Azure login..."
+
+CURRENT_SUBSCRIPTION_ID=$(az account list --query "[?isDefault].id" --output tsv)
+
+if [ -z "$CURRENT_SUBSCRIPTION_ID" ]
+	then 
+		printf '%s\n' "ERROR! Not logged in to Azure. Run az account login" >&2
+#		exit 1
+	else
+		echo "${YELLOW}SUCCESS!${NC}"
+fi
+
+#####################
+#Get Azure Key Vault#
+#####################
+echo "Searching for Terraform KeyVault..."
+KEY_VAULT_NAME=$(az keyvault list --resource-group $TERRAFORM_RESOURCE_GROUP --query "[?contains(name,'$KEY_VAULT_NAME_PATTERN')].name" --output tsv)
+
+if [ -z "$KEY_VAULT_NAME" ]
+	then
+		printf '%s\n' "ERROR! No Azure Key Vault with name pattern like $KEY_VAULT_NAME_PATTERN" >&2
+#		exit 1
+	else
+		echo "${YELLOW}SUCCESS!${NC}"
+fi
+
+#############################
+#Get Azure KeyVault Secrets	#
+#############################
+echo "Loading ARM_SUBSCRIPTION_ID..."
+ARM_SUBSCRIPTION_ID=$(az keyvault secret show --vault-name $KEY_VAULT_NAME --name ARM-SUBSCRIPTION-ID --query "value" --output tsv)
+if [ -z "$ARM_SUBSCRIPTION_ID" ]
+	then 
+		printf '%s\n' "FAILURE! Azure Key Vault missing secret ARM-SUBSCRIPITON-ID" >&2
+	else
+		echo "${YELLOW}SUCCESS!${NC}"
+fi
+
+echo "Loading ARM_CLIENT_ID..."
+ARM_CLIENT_ID=$(az keyvault secret show --vault-name $KEY_VAULT_NAME --name ARM-CLIENT-ID --query "value" --output tsv)
+if [ -z "$ARM_CLIENT_ID" ]
+	then 
+		printf '%s\n' "FAILURE! Azure Key Vault missing secret ARM-CLIENT-ID" >&2
+	else
+		echo "${YELLOW}SUCCESS!${NC}"
+fi
+
+echo "Loading ARM_CLIENT_SECERT"
+ARM_CLIENT_SECRET=$(az keyvault secret show --vault-name $KEY_VAULT_NAME --name ARM-CLIENT-SECRET --query "value" --output tsv)
+if [ -z "$ARM_CLIENT_SECRET" ]
+	then 
+		printf '%s\n' "FAILURE! Azure Key Vault missing secret ARM-CLIENT-SECRET" >&2
+	else
+		echo "${YELLOW}SUCCESS!${NC}"
+fi
+
+echo "Loading ARM_TENANT_ID..."
+ARM_TENANT_ID=$(az keyvault secret show --vault-name $KEY_VAULT_NAME --name ARM-TENANT-ID --query "value" --output tsv)
+if [ -z "$ARM_TENANT_ID" ]
+	then 
+		printf '%s\n' "FAILURE! Azure Key Vault missing secret ARM-TENANT-ID" >&2
+	else
+		echo "${YELLOW}SUCCESS!${NC}"
+fi
+
+echo "Loading ARM_ACCESS_KEY..."
+ARM_ACCESS_KEY=$(az keyvault secret show --vault-name $KEY_VAULT_NAME --name ARM-ACCESS-KEY --query "value" --output tsv)
+if [ -z "$ARM_ACCESS_KEY" ]
+	then 
+		printf '%s\n' "FAILURE! Azure Key Vault missing secret ARM-ACCESS_KEY" >&2
+	else
+		echo "${YELLOW}SUCCESS!${NC}"
+fi
+
+}
+
+function terra_get_backend_values() {
+
+	BACKEND_STORAGE_ACCOUNT=$(az storage account list --resource-group $TERRAFORM_RESOURCE_GROUP --query "[?contains(@.name, 'terraform')==\`true\`].name" --output tsv)
+}
+
+
+function terra_output_info() {
+	echo "************************************************************************"
+	echo "                              SPN VALUES"
+	echo "************************************************************************"
+	echo "ARM_SUBSCRIPTION_ID: $ARM_SUBSCRIPTION_ID"
+	echo "ARM_CLIENT_ID:       $ARM_CLIENT_ID"
+	echo "ARM_CLIENT_SECRET:   HIDDEN!"
+	echo "ARM_TENANT_ID:       $ARM_TENANT_ID"
+	echo "ARM_ACCESS_KEY:      $ARM_ACCESS_KEY"
+	echo "************************************************************************"
+	echo ""
+	export ARM_CLIENT_ID
+	export ARM_CLIENT_SECRET
+	export ARM_TENANT_ID
+	export ARM_SUBSCRIPTION_ID
+	export ARM_ACCESS_KEY
+}
+
+function tofu_init() {
+	
+	TERRAFORM_INIT="tofu init --backend-config='storage_account_name=$BACKEND_STORAGE_ACCOUNT' --backend-config='key=$STATE_KEY' --backend-config='container_name=$STATE_CONTAINER_NAME' --reconfigure"
+	echo "Running: $TERRAFORM_INIT"
+
+	bash -c $TERRAFORM_INIT
+
+}
+#####################
+#		MAIN		#
+#####################
+function tofu_setup() {
+	ENV_FILE=$1
+	echo "ENV_FILE is $ENV_FILE"
+	terra_set_core_variables
+	terra_get_keyvault_values
+	terra_get_backend_values
+	terra_read_env_file
+	terra_output_info
+	tofu_init
+
+	echo "FINISHED!"
+}
+
+function tofu_plan() {
+	eval "tofu plan -var-file $ENV_FILE -out terraform.plan"
+}
+
+function tofu_apply() {
+	eval "tofu apply terraform.plan" 
+}
+
+function tofu_destroy() {
+	eval "tofu apply -destroy -var-file $ENV_FILE" 
+}
